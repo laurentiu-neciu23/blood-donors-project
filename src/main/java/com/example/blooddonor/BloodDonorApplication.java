66564d702b11871293899aec1ab043f09578f812
Neciu Laurentiu-Florin <laurentiuflorinneciu@gmail.com>
2018-11-19 14:17:08 +0200

    Succesfully started base server

--- src/main/java/com/example/blooddonor/BloodDonorApplication.java
+++ src/main/java/com/example/blooddonor/BloodDonorApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.example.blooddonor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BloodDonorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BloodDonorApplication.class, args);
 	}
 }
