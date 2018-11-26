0a64273c0012522b47d8064bfc491401b2d4906e
Neciu Laurentiu-Florin <laurentiuflorinneciu@gmail.com>
2018-11-26 11:31:03 +0200

    better css

--- client/src/App.js
+++ client/src/App.js
@@ -65,11 +65,17 @@ class App extends Component {
 
         </div>
 
-        <div className="account-flex description-position">
-          <div className="spinner-container"></div>
-            <div className="spinner">
-
+        <div className="description-position">
+            <div className="spinner-container">
+              <div className="spinner">
+      
+              </div>
             </div>
+
+            <Typography className="presentation-text">
+            Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a crea o carte demonstrativă pentru literele respective. Nu doar că a supravieţuit timp de cinci secole, dar şi a facut saltul în tipografia electronică practic neschimbată. A fost popularizată în anii '60 odată cu ieşirea colilor Letraset care conţineau pasaje Lorem Ipsum, iar mai recent, prin programele de publicare pentru calculator, ca Aldus PageMaker care includeau versiuni de Lorem Ipsum.
+            </Typography>
+
           </div>
 
         </div>
