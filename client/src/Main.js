ccaf9c67d417c8815463990bfd7ad653ebf78016
thelifeinro <d_pirvan@yahoo.com>
2018-12-08 13:46:12 +0200

    Update Main.js

--- client/src/Main.js
+++ client/src/Main.js
@@ -5,13 +5,19 @@ import { CSSTransition } from "react-transition-group";
 import { NotificationManager, NotificationContainer} from "react-notifications";
 import 'react-notifications/lib/notifications.css';
 import UserProfile from './UserProfile';
+import Summary from './Summary';
+import NewDonation from './NewDonation';
+import Donations from './Donations';
+import Analyses from './Analyses';
+
 
 class Main extends Component {
 
     constructor(props) {
       super(props);
       this.state = {
-        numeleMeu: "Despacito"
+        numeleMeu: "Despacito",
+        toShow: "summary"
       };
     }
 
@@ -19,10 +25,40 @@ class Main extends Component {
         NotificationManager.success("Success", "You have successfully logged in!");
     }
 
+    handleShow = (_toShow) => {
+        this.setState({toShow: _toShow});
+    }
+
+    renderPage = () => {
+        let myComp = null;
+        if(this.state.toShow == "summary"){
+            myComp = <Summary/>
+        }
+        else if(this.state.toShow == "profile") {
+            myComp = <UserProfile/>
+        }
+        else if(this.state.toShow == "analyses") {
+            myComp = <Analyses/>
+        }
+        else if(this.state.toShow == "donations") {
+            myComp = <Donations/>
+        }
+        else if(this.state.toShow == "new-donation") {
+            myComp = <NewDonation/>
+        }
+        return (
+            <div> 
+                {myComp}
+            </div>
+        )
+    }   
+
     render() {
         return (
             <div className="Main">
-                
+                <div>
+                    {this.renderPage()}
+                </div>
                 <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
                     <div className="row-flex pannel">
                         <div className="lds-heart-container">
@@ -32,26 +68,26 @@ class Main extends Component {
                             </div>
                         </div>
                         <div className="button-flex">
-                            <Button color='default' className="Multibutton-root-1 button">
+                            <Button color='default' className="Multibutton-root-1 button" onClick={() => this.handleShow("summary")}>
                                 Dashboard
                             </Button>
-                            <Button color='default' className="Multibutton-root-1 button">
+                            <Button color='default' className="Multibutton-root-1 button" onClick={() => this.handleShow("profile")}>
                                 Profile
                             </Button>
-                            <Button color='default' className="Multibutton-root-1 button">
-                                Analysis
+                            <Button color='default' className="Multibutton-root-1 button" onClick={() => this.handleShow("analyses")}>
+                                Analyses
                             </Button>
-                            <Button color='default' className="Multibutton-root-1 button">
+                            <Button color='default' className="Multibutton-root-1 button" onClick={() => this.handleShow("donations")}>
                                 Donations
                             </Button>
-                            <Button color='default' className="Multibutton-root-1 button">
+                            <Button color='default' className="Multibutton-root-1 button" onClick={() => this.handleShow("new-donation")}>
                                 New Donation
                             </Button>
                         </div>
                     </div>
                     
                 </CSSTransition>
-                <UserProfile className="row-flex"/>
+                
                 <NotificationContainer />
             </div>
 
